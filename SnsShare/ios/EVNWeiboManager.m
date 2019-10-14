05c300afc771dc1249902518932582ca4511905b
xiaoxinbo <xiaoxinbo@ifunq.com>
2019-10-14 17:03:38 +0800

    bug fix

--- SnsShare/ios/EVNWeiboManager.m
+++ SnsShare/ios/EVNWeiboManager.m
@@ -50,16 +50,16 @@
     }
   }];
   [_reqQueue removeAllObjects];
-  
+
 }
 
-- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
 {
   BOOL isSuccess = [WeiboSDK handleOpenURL:url delegate:self];
   if (isSuccess) {
     [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(delayCheckAlipaySDKCallbacks) object:nil];
   }
-  
+
   return  isSuccess;
 }
 
@@ -81,7 +81,7 @@
 
 /**
  收到一个来自微博客户端程序的请求
- 
+
  收到微博的请求后，第三方应用应该按照请求类型进行处理，处理完后必须通过 [WeiboSDK sendResponse:] 将结果回传给微博
  @param request 具体的请求对象
  */
@@ -92,7 +92,7 @@
 
 /**
  收到一个来自微博客户端程序的响应
- 
+
  收到微博的响应后，第三方应用可以通过响应类型、响应的数据和 WBBaseResponse.userInfo 中的数据完成自己的功能
  @param response 具体的响应对象
  */
